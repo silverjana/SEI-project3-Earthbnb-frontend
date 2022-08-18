cb591650125e4e8e1cda2cda59b757305fa96d03
Arianna Giordano <silverjana@live.com>
2022-08-18 15:08:27 +0100

    "imports and components"
    "imports and components"

---
+++ src/components/AddReview.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const AddReview = () => {
+  return (
+
+    <>
+      <h1>You can add a review here</h1>
+      {/* add button to go back */}
+      <Link to="/">
+        <button>Back to Home</button>
+      </Link>
+    </>
+  )
+}
+
+export default AddReview
