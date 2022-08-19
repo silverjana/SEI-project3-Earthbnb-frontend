d2d12a98ffaacf9d43dff72dc37ef2e2712cb533
florastocks <stocksflora@gmail.com>
2022-08-19 19:33:01 +0100

    styling edits to homepage

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,26 @@
 import { Link } from 'react-router-dom'
+import Cityimage from '../images/city-homepage.jpeg'
+import Countryimage from '../images/countryside-homepage.jpeg'
+import Islandimage from '../images/island-homepage.jpeg'
+
+// const buttonImages = [
+//   {
+//     url:'city-homepage.jpeg',
+//     title: 'Head to the City',
+//     width: '30%',
+//   },
+//   {
+//     url:'countryside-homepage.jpeg',
+//     title: 'Excape to the Country',
+//     width: '30%',
+//   },
+//   {
+//     url:'island-homepage.jpeg',
+//     title: 'Jump to an Island',
+//     width: '30%',
+//   }
+
+// ]
 
 const Landing = () => {
   return (
@@ -6,10 +28,26 @@ const Landing = () => {
       <main className="hero text-center">
         <h1>Earth BnB</h1>
         <p className='lead'>Plan you next sustainable holiday by choosing our green properties</p>
-        <Link className="btn" to='/allproperties'>Find your next Holiday</Link>
+        <section className='btn-section'>
+          <div className='btn-container'>
+            <img src={Cityimage} alt='city'/>
+            <button className='btn-homepage'>Head to the City</button>
+          </div>
+          <div className='btn-container'>
+            <img src={Countryimage} alt='country'/>
+            <button className='btn-homepage'>Escape to the Country</button>
+          </div>
+          <div className='btn-container'>
+            <img src={Islandimage} alt='island'/>
+            <button className='btn-homepage'>Pop to an Island</button>
+          </div>
+        </section>
+          <Link className="btn" to='/allproperties'>Find your next Holiday</Link>
+        {/* </div> */}
       </main>
     </div>
   )
 }
+
 
 export default Landing
