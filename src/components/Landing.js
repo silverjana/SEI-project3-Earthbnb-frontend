137a32f308cbb187e647f30d0cdeec0bcbd4b691
florastocks <stocksflora@gmail.com>
2022-08-20 19:35:36 +0100

    hover affects, and slides

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import Cityimage from '../images/city-homepage.jpeg'
 import Countryimage from '../images/countryside-homepage.jpeg'
 import Islandimage from '../images/island-homepage.jpeg'
-
+import { Carousel } from 'react-bootstrap'
 // const buttonImages = [
 //   {
 //     url:'city-homepage.jpeg',
@@ -28,20 +28,26 @@ const Landing = () => {
       <main className="hero text-center">
         <h1>Earth BnB</h1>
         <p className='lead'>Plan you next sustainable holiday by choosing our green properties</p>
-        <section className='btn-section'>
-          <div className='btn-container'>
-            <img src={Cityimage} alt='city'/>
-            <button className='btn-homepage'>Head to the City</button>
-          </div>
-          <div className='btn-container'>
-            <img src={Countryimage} alt='country'/>
+        <Carousel className='carousel'>
+          <Carousel.Item interval={(1000) * 8}>
+            <img className="carousel-image"  src={Cityimage} alt='city'/>
+            <Carousel.Caption>
+              <button className='btn-homepage'>Head to the City</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={(1000) * 8}>
+          <img className="carousel-image" src={Countryimage} alt='country'/>
+            <Carousel.Caption>
             <button className='btn-homepage'>Escape to the Country</button>
-          </div>
-          <div className='btn-container'>
-            <img src={Islandimage} alt='island'/>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={(1000) * 8}>
+          <img className="carousel-image" src={Islandimage} alt='island'/>
+            <Carousel.Caption>
             <button className='btn-homepage'>Pop to an Island</button>
-          </div>
-        </section>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
           <Link className="btn" to='/allproperties'>Find your next Holiday</Link>
         {/* </div> */}
       </main>
@@ -51,3 +57,20 @@ const Landing = () => {
 
 
 export default Landing
+
+
+{/* <section className='btn-section'>
+<div className='btn-container'>
+  <img src={Cityimage} alt='city'/>
+  <button className='btn-homepage'>Head to the City</button>
+</div>
+<div className='btn-container'>
+  <img src={Countryimage} alt='country'/>
+  <button className='btn-homepage'>Escape to the Country</button>
+</div>
+<div className='btn-container'>
+  <img src={Islandimage} alt='island'/>
+  <button className='btn-homepage'>Pop to an Island</button>
+</div>
+</section>
+<Link className="btn" to='/allproperties'>Find your next Holiday</Link> */}
