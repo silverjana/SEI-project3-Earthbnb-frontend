2590db217b642dac1fe2011c97619ff1e6169163
florastocks <stocksflora@gmail.com>
2022-08-21 17:21:29 +0100

    can fetch data from backend api

--- src/components/AllProperties.js
+++ src/components/AllProperties.js
@@ -1,5 +1,51 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import { Link } from "react-router-dom"
+
 const AllProperties = () => {
-  return <h1>All Properties Page</h1> 
+
+  const [ allProps, setAllProps ] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('https://project3-earthbnb.herokuapp.com/allproperties')
+        setAllProps(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
+  console.log(allProps)
+
+  return (
+    <Container as='main'>
+      <Row>
+        {allProps.map(property => {
+          const { _id, name, type, price } = property
+          console.log(property)
+          return (
+            <Col key={_id}>
+              <Link to={`/allproperties/${_id}`}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title className="card-title">{name}, {type} - {price}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          )
+        })}
+
+      </Row>
+    </Container>
+  )
 }
 
 export default AllProperties
--- src/components/SingleProperty.js
+++ src/components/SingleProperty.js
@@ -1,4 +1,29 @@
+// import { useState, useEffect } from "react"
+// import axios from "axios"
+// import { useParams, Link } from "react-router-dom"
+
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+
 const SingleProperty = () => {
+
+  // const { propertyId } = useParams()
+  // const [ property, setProperty ] = useState(null)
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const { data } = await axios.get(`https://project3-earthbnb.herokuapp.com/allproperties/${propertyId}`)
+  //       setProperty(data)
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   getData()
+  // }, [propertyId])
+
+  // console.log(propertyId)
   return <h1>Single Property Page</h1>
 }
 
