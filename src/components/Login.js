a660a528f5038f68e0a03c911a2415000d2aa491
Arianna Giordano <silverjana@live.com>
2022-08-22 16:30:06 +0100

    login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,66 @@
 import { TextField } from '@mui/material'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-
-//add handle changes 
-//and onclick 
 const Login = () => {
+
+
+  const [data, setData] = useState({
+    userName: "",
+    password: "",
+  })
+
+  const navigate = useNavigate()
+
+  const [error, setError] = useState('')
+
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value })
+    setError('')
+  }
+
+  //
+  const onSubmit = async (event) => {
+
+    event.preventDefault()
+
+    try {
+      // API request -> POST req to login
+      const res = await axios.post("https://project3-earthbnb.herokuapp.com/login", data)
+      //setError(null)
+      //token is the response
+      const { token } = res.data
+      //set in local storage
+      localStorage.setItem("EarthBnbToken", token)
+      //put token in header for all requests, with bearer
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+      //go to 
+      navigate("/allproperties")
+
+    } catch (error) {
+      console.log(error)
+
+      setError(error.response.data.message)
+
+      //! error on forms
+      //helperText={error ? "Incorrect entry" : false}
+      //error={error ? true : false }
+      
+    }
+  }
+
   return (
     <main className='form-page'>
       <Container>
         <Row>
-          <form className='form'>
+          <form className='form' onSubmit={onSubmit}>
             <h3 className="text-center">Login</h3>
-            <TextField required className="form-input" id="outlined-required" name='email' label="Email"/>
-            <TextField required className="form-input" id="outlined-password-input" type="password" name='password' label="Password"/>
+            <TextField error={error ? true : false } required className="form-input" id="outlined-required" name='userName' label="Username" value={data.userName} onChange={handleChange} />
+            <TextField error={error ? true : false } required className="form-input" id="outlined-password-input" type="password" name='password' label="Password" value={data.password} onChange={handleChange} />
+            {error && <div className='error-mex'>{error}</div>}
             <input type="submit" value="Login" className='btn dark w-100'/>
           </form>
         </Row>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,21 +1,81 @@
 //! Imports
-import { TextField } from "@mui/material"
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
 
+import { TextField } from '@mui/material'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+
+  const [data, setData] = useState({
+    email: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const navigate = useNavigate()
+
+  const [error, setError] = useState('')
+  const [pwError, setPwError] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [userError, setUserError] = useState('')
+
+  //update data with each change
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value })
+    //reset error
+    setError('')
+    setPwError('')
+    setEmailError('')
+    setUserError('')
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      // API request -> POST req to login
+      await axios.post("https://project3-earthbnb.herokuapp.com/register", data)
+      //setError(null)
+      //go to 
+      navigate("/login")
+
+    } catch (error) {
+      console.log(error)
+
+      setError(error.response.data.message)
+
+      if (error.response.data.message === 'Passwords do not match.') {
+        setPwError('yep')
+      }
+      if (error.response.data.message === "User with this email already exists") {
+        setEmailError('yep')
+      }
+      if (error.response.data.message === "User with this username already exists") {
+        setUserError('yep')
+      }
+
+
+    }
+  }
+
+
+
   return (
     <main className="form-page">
       <Container>
         <Row>
-          <form className="form">
+          <form className="form" onSubmit={onSubmit}>
             <h3 className="text-center">Register</h3>
             {/* add value={formData.username; .email; .password; .passwordConfirmation} */}
-            <TextField required className="form-input" id="outlined-required" name='username' label="Username"/>
-            <TextField required className="form-input" id="outlined-required" name='email' label="Email"/>
-            <TextField required className="form-input" id="outlined-password-input" type="password" name='password' label="Password"/>
-            <TextField required className="form-input" id="outlined-password-input" type="password" name='passwordConfirmation' label="Confirm Password"/>
+            <TextField required error={userError ? true : false} className="form-input" id="outlined-required" name='userName' label="Username" value={data.userName} onChange={handleChange} />
+            <TextField required error={emailError ? true : false} className="form-input" id="outlined-required 2" name='email' label="Email" value={data.email} onChange={handleChange} />
+            <TextField required error={pwError ? true : false} className="form-input" id="outlined-password-input" type="password" name='password' label="Password" value={data.password} onChange={handleChange} />
+            <TextField required error={pwError ? true : false} className="form-input" id="outlined-password-input 2" type="password" name='confirmPassword' label="Confirm Password" value={data.confirmPassword} onChange={handleChange} />
+            {error && <div className='error-mex'>{error}</div>}
             <input type="submit" value="Register" className='btn dark w-100' />
           </form>
         </Row>
@@ -23,4 +83,5 @@ const Register = () => {
     </main>
   )
 }
+
 export default Register
