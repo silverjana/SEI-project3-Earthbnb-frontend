2bff426d0904a497869fe99ffb4ab58d6d5e1a7a
florastocks <stocksflora@gmail.com>
2022-08-23 16:40:09 +0100

    added property decription section on individual property page

--- src/components/SingleProperty.js
+++ src/components/SingleProperty.js
@@ -25,32 +25,35 @@ const SingleProperty = () => {
     getData()
   }, [id]) 
 
-  const imageOne = []
-  
   console.log(id)
 
   return (
     <Container as='main'>
       { property ?
-        <Row> 
-          <>
-            <h1 className="property-heading">{property.name}</h1>
-            <div className="image-container">
-                {property.images.map(image => {
-                  return (
-                      <img className="indiv-img"src={image} alt={property.name}/>
-                  )
-                })}
-            </div>
-          </>
-        </Row>
-      :
-      <h2>
-        { errors ? 'Something went wrong, Please try again Later' : 'Loading...'}
-      </h2>
+        <>
+          <Row>
+              <h1 className="property-heading">{property.name}</h1>
+              <div className="image-container">
+                  {property.images.map((image, idx) => {
+                    return (
+                        <img key={idx}className="indiv-img"src={image} alt={property.name}/>
+                    )
+                  })}
+              </div>
+          </Row>
+          <Row>
+            <section className="description-container">
+              <h3 className='descript-heading'>Property Description:</h3>
+              <p className='description-para'>{property.description}</p>
+            </section>
+          </Row>
+        </>
+        :
+        <h2>
+          { errors ? 'Something went wrong, Please try again Later' : 'Loading...'}
+        </h2>
       }
     </Container>
-    
   )
 }
 
